87d26058ca04786dad42d4a8b356ccbedda519ee
whu3 <whu3@cs.washington.edu>
2024-06-09 22:16:14 -0700

    Added Basic Movement Script

--- Project WEE/Assets/Scripts/ThirdPersonMovement.cs
+++ Project WEE/Assets/Scripts/ThirdPersonMovement.cs
@@ -8,11 +8,22 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public float speed = 6f;
 
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            // use atan to determine angle, result in radians
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            // wasd movement
+            controller.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
