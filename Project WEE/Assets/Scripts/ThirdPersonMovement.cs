f02e97c6ba3a3cf826e8fa5a855ea2c03baf8c31
whu3 <whu3@cs.washington.edu>
2024-06-09 21:41:19 -0700

    Created Third Person Movment Blank Script

---
+++ Project WEE/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
